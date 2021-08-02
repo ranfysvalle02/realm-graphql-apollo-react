b2d0883451a4e7fa2d8c6b3a1862026d24e84e71
Fabian Valle <ranfys.valle@gmail.com>
2021-08-02 15:45:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./styles.css";
 import * as React from "react";
-import { APP_ID } from "./index";
+import * as Realm from "realm-web";
 import { useQuery, useMutation } from "@apollo/client";
 import { FIND_MOVIE, UPDATE_MOVIE } from "./graphql-operations";
 
 export default function App(props) {
+  const APP_ID = process.env.APP_ID;
+
+  // Connect to your MongoDB Realm app
+  const app = new Realm.App(APP_ID);
+
   const [searchText, setSearchText] = React.useState("The Matrix Reloaded");
   const { loading, data } = useQuery(FIND_MOVIE, {
-    variables: { query: { title: searchText } }
+    variables: { query: { title: searchText } },
+    pollInterval: 500
   });
 
   const movie = data ? data.movie : null;
   const [updateMovie, { loading: updating }] = useMutation(UPDATE_MOVIE);
   const [newTitleText, setNewTitleText] = React.useState("Silly New Title");
 
+  const [loginEmail, setLoginEmail] = React.useState("example123@gmail.com");
+  const [loginEmailPwd, setLoginEmailPwd] = React.useState("example123@gmail.com");
+  
+  const [registerEmail, setRegisterEmail] = React.useState("example123@gmail.com");
+  const [registerEmailPwd, setRegisterEmailPwd] = React.useState("example123@gmail.com");
+  
   const updateMovieTitle = async () => {
     if (!movie) return;
     await updateMovie({
@@ -24,9 +36,102 @@ export default function App(props) {
     });
     setSearchText(newTitleText);
   };
+  
+  async function registerUser(email,password){
+    return await app.emailPasswordAuth.registerUser(email, password).then((xx)=>{console.log('que?',xx)});
+  }
+  async function loginEmailPassword(email, password) {
+    // Create an anonymous credential
+    const credentials = Realm.Credentials.emailPassword(email, password);
+    try {
+      // Authenticate the user
+      const user = await app.logIn(credentials);
+      return user
+    } catch(err) {
+      console.error("Failed to log in", err);
+    }
+  }
+  async function tryToLoginAnonymously(){
+    return await app.logIn(Realm.Credentials.anonymous());
+  }
+  const tryToLogin = async function(){
+    try{
+      const realmUser = await loginEmailPassword(loginEmail,loginEmailPwd);
+      console.log('realmUser',realmUser);
+      localStorage.setItem('token',realmUser['_accessToken']);
+    }catch(e){
+      // double login theoretically works without requiring a logout
+      // TODO: double login does not trigger an error. find out how to track auth status elegantly
+      console.log('e-login',e);
+    }
+  };
 
+  const tryToRegister = async function(){
+    try{
+      await registerUser(registerEmail,registerEmailPwd);
+    }catch(e){
+      console.log('error',e);
+      alert('Could not register the user');
+    }
+  };
+
+  
   return (
     <div className="App">
+      <h1>Sign in Anonymously</h1>
+      <div className="title-input">
+        <button
+          className="fancy-button"
+          onClick={() => tryToLoginAnonymously()}
+        >
+          Login Anonymously
+        </button>
+      </div>
+      <hr /> 
+      <h1>OR Register</h1>
+      <div className="title-input">
+        <input
+          type="text"
+          className="fancy-input"
+          value={registerEmail}
+          onChange={e => setRegisterEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="fancy-input"
+          value={registerEmailPwd}
+          onChange={e => setRegisterEmailPwd(e.target.value)}
+        />
+        <button
+          className="fancy-button"
+          onClick={() => tryToRegister()}
+        >
+          Register with E-mail
+        </button>
+      </div>
+      <hr /> 
+      <h1>Then Login</h1>
+      <div className="title-input">
+        <input
+          type="text"
+          className="fancy-input"
+          value={loginEmail}
+          onChange={e => setLoginEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          className="fancy-input"
+          value={loginEmailPwd}
+          onChange={e => setLoginEmailPwd(e.target.value)}
+        />
+        <button
+          className="fancy-button"
+          onClick={() => tryToLogin()}
+        >
+          Sign-In with E-mail
+        </button>
+      </div>
+      <hr /> 
       <h1>Find a Movie</h1>
       <span className="subheading">
         The app automatically searches as you type
