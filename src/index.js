3c34cc26d4e98bbb87a1b070b1c65726b43726d2
Fabian Valle <ranfys.valle@gmail.com>
2021-08-02 15:45:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ import {
   HttpLink,
   InMemoryCache,
 } from "@apollo/client";
-// Realm
-import * as Realm from "realm-web";
 // Check out app.js for examples of how to run GraphQL operations
 import App from "./App";
 
@@ -23,36 +21,16 @@ import App from "./App";
 // 5. Deploy your changes
 //
 // Once your app is set up, replace the value of APP_ID with your App ID
-export const APP_ID = "<Your App ID>";
+export const APP_ID = process.env.APP_ID;
 
-// Connect to your MongoDB Realm app
-const app = new Realm.App(APP_ID);
-
-// Gets a valid Realm user access token to authenticate requests
-async function getValidAccessToken() {
-  // Guarantee that there's a logged in user with a valid access token
-  if (!app.currentUser) {
-    // If no user is logged in, log in an anonymous user. The logged in user will have a valid
-    // access token.
-    await app.logIn(Realm.Credentials.anonymous());
-  } else {
-    // An already logged in user's access token might be stale. To guarantee that the token is
-    // valid, we refresh the user's custom data which also refreshes their access token.
-    await app.currentUser.refreshCustomData();
-  }
-  
-  return app.currentUser.accessToken;
-}
-
-// Configure the ApolloClient to connect to your app's GraphQL endpoint
-const client = new ApolloClient({
+let client = new ApolloClient({
   link: new HttpLink({
     uri: `https://realm.mongodb.com/api/client/v2.0/app/${APP_ID}/graphql`,
     // We define a custom fetch handler for the Apollo client that lets us authenticate GraphQL requests.
     // The function intercepts every Apollo HTTP request and adds an Authorization header with a valid
     // access token before sending the request.
-    fetch: async (uri, options) => {
-      const accessToken = await getValidAccessToken();
+    fetch: (uri, options) => {
+      const accessToken = localStorage.getItem('token');
       options.headers.Authorization = `Bearer ${accessToken}`;
       return fetch(uri, options);
     },
